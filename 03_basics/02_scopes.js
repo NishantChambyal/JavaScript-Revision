3253889d1b31deb5231f496b0b3f34aaca9c1306
NishantChambyal <146934202+NishantChambyal@users.noreply.github.com>
2024-11-03 05:45:23 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,8 +4,57 @@ if (true) {
   let a = 100;
   const b = 200;
   var c = 400;
-  console.log("inner;", a);
+  // console.log("inner;", a);
   
 }
 
-console.log(a);
+// console.log(a);
+
+
+
+//+++++++++++++++++++ scope functions (closers) +++++++++++++++++++++
+
+function one() {
+  const username = "nishant"
+
+  function two() {
+    const website = "youtube"
+    console.log(username);
+    
+  }
+  // console.log(website);
+  
+  two()
+}
+
+one()
+
+
+if (true) {
+  const username = "hitesh"
+
+  if (username === "hitesh") {
+    const website = " youtube"
+    console.log(username + website);
+  }
+  // console.log(website);
+  
+}
+// console.log(username);
+
+// ++++++++++++++++++++++++ intersting ++++++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num) {
+  return num + 1
+}
+
+
+
+
+addTwo(5) // error because not exicute before declaration
+
+const addTwo = function (num) {          // expression function (variable (named function))
+  return num + 2
+}
